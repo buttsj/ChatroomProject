04a1338187be4b4c5bb8aa0fd13c1d96357db01d
Jack Butts <butts.102@osu.edu>
2016-02-18 14:05:53 -0500

    Added account activity (page)

--- app/src/main/java/net/dreameater/chatroomproject/AccountActivity.java
+++ app/src/main/java/net/dreameater/chatroomproject/AccountActivity.java
@@ -1,26 +1,27 @@
 package net.dreameater.chatroomproject;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class LobbyActivity extends AppCompatActivity {
+public class AccountActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lobby);
+        setContentView(R.layout.activity_account);
 
         // store the top bar here
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         assert getSupportActionBar() != null;
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setTitle("Lobby");
-
+        getSupportActionBar().setTitle("Account Settings");
     }
 
     @Override
@@ -29,17 +30,13 @@ public class LobbyActivity extends AppCompatActivity {
             case android.R.id.home:
                 finish();
                 return true;
-            case R.id.action_settings:
-                startActivity(new Intent(LobbyActivity.this, AccountActivity.class));
-                return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_lobby, menu);
+
         return true;
     }
-
 
 }
