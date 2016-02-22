c4049a002e65f49c8ad664a2f09c01da96dade7c
Jack Butts <butts.102@osu.edu>
2016-02-21 21:29:23 -0500

    added Galaxy Note 3 image for Account page

--- app/src/main/java/net/dreameater/chatroomproject/AccountActivity.java
+++ app/src/main/java/net/dreameater/chatroomproject/AccountActivity.java
@@ -5,13 +5,17 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 
 // THIS IS YOUR ACCOUNT INFORMATION PAGE
 
 public class AccountActivity extends AppCompatActivity {
+
+    String phoneModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +28,13 @@ public class AccountActivity extends AppCompatActivity {
         assert getSupportActionBar() != null;
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Account Settings");
+        phoneModel = android.os.Build.MODEL;
+
+        if (phoneModel.contains("Galaxy Note"))
+        {
+            ImageView iv=(ImageView) findViewById(R.id.galaxynote3View);
+            iv.setVisibility(View.VISIBLE);
+        }
     }
 
     @Override
@@ -36,9 +47,6 @@ public class AccountActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public boolean onCreateOptionsMenu(Menu menu) {
-
-        return true;
-    }
+    public boolean onCreateOptionsMenu(Menu menu) { return true;}
 
 }
